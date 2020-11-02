72b2db9beb52ab0246cd75ef66a47b3af500a6f4
Liz Kiser <ekiser9@uncc.edu>
2020-11-02 18:37:47 -0500

    Added put route in server.js to add new budget models

--- server.js
+++ server.js
@@ -2,27 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-// const fs = require('fs');
 
 app.use(cors());
 const mongoose = require("mongoose");
 const budgetModel = require("./models/budget_schema");
 let url = "mongodb://localhost:27017/personalbudget";
 
-// let rawdata = fs.readFileSync('budgetdata.json');
-// let budget = JSON.parse(rawdata);
-
 app.use('/', express.static('public'));
 app.use(express.json());
 
 app.get('/budget', (req, res) => {
-    // res.json(budget);
 
     mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true})
         .then(() =>{
             budgetModel.find({})
                 .then((data) => {
-                    console.log("Mongoose worked.")
+                    console.log("Mongoose get working")
                     res.json(data);
                     mongoose.connection.close();
                 })
@@ -33,6 +28,28 @@ app.get('/budget', (req, res) => {
         .catch((connectionError) => {
             console.log(connectionError);
         })
+});
+
+app.put('/budget', (req,res) => {
+    mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true})
+    .then(() => {
+        console.log("Mongoose put working")
+        let newData = new budgetModel({
+            title: req.body.title,
+            budget: req.body.budget,
+            color: req.body.color
+        });
+        budgetModel.insertMany(newData)
+            .then((data) => {
+                mongoose.connection.close();
+            })
+            .catch((connectionError) => {
+                console.log(connectionError);
+            })
+    })
+    .catch((connectionError) => {
+        console.log(connectionError);
+    })
 });
 
 app.listen(port, () => {
